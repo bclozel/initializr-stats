4d455a626bffb383373b088388876bc980c64ce3
Brian Clozel <bclozel@pivotal.io>
2018-09-11 21:30:25 +0200

    Fix Spring Security Configuration
    As mentioned in the Spring Boot 2.0 Migration Guide (see
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.0-Migration-Guide#security)
    the auto-configuration of Spring Security has changed a lot.
    
    Instead of relying on layers of security configurations and specific
    ordering for customizations, we're using now Spring Security defaults
    and providing utilities to easily configure security to your needs.
    
    Because there is no longer a separate Actuator security configuration
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.0-Migration-Guide#actuator-security
    the `management.security.roles` configuration key has been removed.
    
    There's also an easier way to configure endpoint security on Actuator,
    using `EndpointRequest`. Same thing for allowing common static resources
    locations - with `PathRequest` this time.
    
    With the new Spring Security version, password encoding is mandatory;
    for demo purposes, we'll use a "NoOp" encoder for our passwords.
    This is obviously not a safe production option.
    
    For more information on Spring Security migration, you can also check
    out the dedicated wiki page:
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-Security-2.0

--- dashboard/src/main/java/io/spring/sample/dashboard/SecurityConfig.java
+++ dashboard/src/main/java/io/spring/sample/dashboard/SecurityConfig.java
@@ -1,31 +1,36 @@
 package io.spring.sample.dashboard;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+				.requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
+				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
 				.anyRequest().authenticated()
 				.and().formLogin().loginPage("/login").permitAll()
 				.and().httpBasic();
 	}
 
 	@Bean
+	@SuppressWarnings("deprecation")
 	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 		return new InMemoryUserDetailsManager(
-				User.builder().username("user").password("user").roles("USER").build(),
-				User.builder().username("admin").password("admin").roles("USER", "ADMIN").build()
+				User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build(),
+				User.withDefaultPasswordEncoder().username("admin").password("admin").roles("USER", "ADMIN").build()
 		);
 	}
 
