8f84101a63058a86a892e9945a9cc4227bd2b301
Brian Clozel <bclozel@pivotal.io>
2018-09-12 00:23:51 +0200

    Upgrade to Spring Boot 2.1
    Upgrading to Spring Boot 2.1 is quite easy here:
    
    * we can remove the common tag code since it can be covered now
    by configuration properties
    * `WebClient` instances are now producing metrics

--- dashboard/src/main/java/io/spring/sample/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/io/spring/sample/dashboard/DashboardApplication.java
@@ -1,11 +1,7 @@
 package io.spring.sample.dashboard;
 
-import io.micrometer.core.instrument.MeterRegistry;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DashboardApplication {
@@ -14,9 +10,4 @@ public class DashboardApplication {
 		SpringApplication.run(DashboardApplication.class, args);
 	}
 
-	@Bean
-	public MeterRegistryCustomizer<MeterRegistry> commonTags() {
-	   return registry -> registry.config()
-			   .commonTags("application", "initializr-dashboard");
-	}
 }
