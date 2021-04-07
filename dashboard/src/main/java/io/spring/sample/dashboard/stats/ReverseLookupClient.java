1a613f81e3dc7c04b3f1fda8c2b7410c21084b63
Brian Clozel <bclozel@vmware.com>
2021-04-07 21:52:36 +0200

    Add custom metric for rate limit remaining count
    Now that we have Micrometer set up, we can create an additional,
    custom metric for our application. In this case, we'd like to
    track the remaining calls to the rate-limited API we're using.

--- dashboard/src/main/java/io/spring/sample/dashboard/stats/ReverseLookupClient.java
+++ dashboard/src/main/java/io/spring/sample/dashboard/stats/ReverseLookupClient.java
@@ -1,9 +1,14 @@
 package io.spring.sample.dashboard.stats;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.micrometer.core.instrument.MeterRegistry;
 import io.spring.sample.dashboard.stats.support.ReverseLookupDescriptor;
 import reactor.core.publisher.Mono;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
@@ -11,8 +16,8 @@ public class ReverseLookupClient {
 
 	private final WebClient client;
 
-	public ReverseLookupClient(WebClient.Builder builder) {
-		this.client = builder.build();
+	public ReverseLookupClient(WebClient.Builder builder, MeterRegistry registry) {
+		this.client = builder.filter(rateLimitRemainingMetric(registry)).build();
 	}
 
 	public Mono<ReverseLookupDescriptor> freeReverseLookup(String ip) {
@@ -23,5 +28,18 @@ public class ReverseLookupClient {
 	public Mono<ReverseLookupDescriptor> payingReverseLookup(String ip) {
 		return this.client.get().uri("http://localhost:8081/reverse-lookup/costly/{ip}", ip)
 				.retrieve().bodyToMono(ReverseLookupDescriptor.class);
+	}
+
+	private ExchangeFilterFunction rateLimitRemainingMetric(MeterRegistry registry) {
+		AtomicInteger rateLimitRemaining = registry
+				.gauge("reverselookup.ratelimit.remaining", new AtomicInteger(0));
+		return (request, next) -> next.exchange(request)
+				.doOnNext(response -> {
+					String remaining = response.headers().asHttpHeaders()
+							.getFirst("X-RateLimit-Remaining");
+					if (StringUtils.hasText(remaining)) {
+						rateLimitRemaining.set(Integer.parseInt(remaining));
+					}
+				});
 	}
 }
